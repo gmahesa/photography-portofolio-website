3317ab10b1d82a93b9e08546e562c1cdae1ea43d
gmahesa <galangmahesa99@gmail.com>
2022-09-02 09:44:44 +0800

    first commit

---
+++ main.js
@@ -0,0 +1,9 @@
+function onOffNav() {
+    var element = document.getElementById("navBar");
+    element.classList.toggle("show");
+}
+  
+function closeNav() {
+    var element = document.getElementById("navBar");
+    element.classList.remove("show");
+}
